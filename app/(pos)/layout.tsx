6d1df047f711a352ec90323632e38f3b894a88e9
Andrew <ibrahimjindybuisness@gmail.com>
2025-04-28 10:46:22 +0300

    side bar posadder

--- app/(pos)/layout.tsx
+++ app/(pos)/layout.tsx
@@ -1,9 +1,20 @@
 import { POSProvider } from "@/providers/POSProvider";
+import { Permission } from "@/lib/permissions";
+import POSSidebar from "@/components/PosSidebar";
 
 export default function Layout({
   children,
+  additionalPermissions = [],
 }: Readonly<{
   children: React.ReactNode;
+  additionalPermissions?: Permission[];
 }>) {
-  return <POSProvider>{children}</POSProvider>;
+  return (
+    <POSProvider>
+      <div className="fixed">
+      <POSSidebar additionalPermissions={additionalPermissions} />
+      </div>
+      {children}
+    </POSProvider>
+  );
 }
