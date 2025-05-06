baf36101e5af59e537ef973f561ead51989596c7
Ibrahim <ibrahimjindybuisness@gmail.com>
2025-05-06 21:39:11 +0300

    dashboard updated

--- actions/accounting/transactions/get-registere-transaction-by-id.ts
+++ actions/accounting/transactions/get-registere-transaction-by-id.ts
@@ -1,42 +1,48 @@
 "use server";
 
-import { checkUserPermissions } from "@/actions/users/check-permissions";
-import { rolePermissions, UserRole } from "@/lib/permissions";
-import { prisma } from "@/lib/prisma";
-import { decimalToNumber } from "@/lib/utils";
+import {checkUserPermissions} from "@/actions/users/check-permissions";
+import {rolePermissions, UserRole} from "@/lib/permissions";
+import {prisma} from "@/lib/prisma";
+import {decimalToNumber} from "@/lib/utils";
 
 export const getRegisterTransactionById = async (id: string) => {
-  try {
-    await checkUserPermissions(rolePermissions[UserRole.MANAGER]);
+    try {
+        await checkUserPermissions(rolePermissions[UserRole.MANAGER]);
 
-    const register = await prisma.registerTransaction.findUnique({
-      where: {
-        id,
-      },
-      include: {
-        cashier: {
-          select: {
-            id: true,
-            name: true,
-          },
-        },
-      },
-    });
-    if (!register) throw new Error("Could not find transaction");
+        const register = await prisma.registerTransaction.findUnique({
+            where: {
+                id,
+            },
+            include: {
+                cashier: {
+                    select: {
+                        id: true,
+                        name: true,
+                    },
+                },
+                register: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                }
+            },
+        });
+        if (!register) throw new Error("Could not find transaction");
 
-    const formattedRegister = {
-      ...register,
-      created_at: register.created_at
-        ? new Date(register.created_at)
-            .toISOString()
-            .replace("T", " ")
-            .slice(0, 19) // Format: YYYY-MM-DD HH:mm:ss
-        : null,
-    };
+        const formattedRegister = {
+            ...register,
+            created_at: register.created_at
+                ? new Date(register.created_at)
+                    .toISOString()
+                    .replace("T", " ")
+                    .slice(0, 19) // Format: YYYY-MM-DD HH:mm:ss
+                : null,
+        };
 
-    return decimalToNumber(formattedRegister);
-  } catch (error) {
-    console.error("Error fetching transactions:", error);
-    throw new Error("Failed to fetch transactions");
-  }
+        return decimalToNumber(formattedRegister);
+    } catch (error) {
+        console.error("Error fetching transactions:", error);
+        throw new Error("Failed to fetch transactions");
+    }
 };
--- lib/transactions-service.ts
+++ lib/transactions-service.ts
@@ -1,43 +1,52 @@
-import { createRegisterTransaction } from "@/actions/accounting/transactions/create-register-transaction";
-import { getAllRegisterTransactions } from "@/actions/accounting/transactions/get-all-register-transactions";
-import { getRegisterTransactionById } from "@/actions/accounting/transactions/get-registere-transaction-by-id";
-import { RegisterTransaction } from "@prisma/client";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {createRegisterTransaction} from "@/actions/accounting/transactions/create-register-transaction";
+import {getAllRegisterTransactions} from "@/actions/accounting/transactions/get-all-register-transactions";
+import {getRegisterTransactionById} from "@/actions/accounting/transactions/get-registere-transaction-by-id";
+import {RegisterTransaction} from "@prisma/client";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 
 export const useTransactions = () => {
     return useQuery({
-      queryKey: ["transactions"],
-      queryFn: async () => {
-        const response = await getAllRegisterTransactions();
-        return response;
-      },
+        queryKey: ["transactions"],
+        queryFn: async () => {
+            const response = await getAllRegisterTransactions();
+            return response;
+        },
     });
-  };
-  
-  export const useTransaction = (id: string) => {
+};
+
+export const useTransaction = (id: string) => {
     return useQuery({
-      queryKey: ["transactions", id],
-      queryFn: async () => {
-        const response = (await getRegisterTransactionById(
-          id
-        )) as RegisterTransaction;
-        return response;
-      },
-      enabled: !!id,
+        queryKey: ["transactions", id],
+        queryFn: async () => {
+            const response = (await getRegisterTransactionById(
+                id
+            )) as RegisterTransaction & {
+                cashier: {
+                    name: string
+                    id: string
+                },
+                register: {
+                    name: string
+                    id: string
+                }
+            };
+            return response;
+        },
+        enabled: !!id,
     });
-  };
-  
-  export const useCreateTransaction = () => {
+};
+
+export const useCreateTransaction = () => {
     const queryClient = useQueryClient();
-  
+
     return useMutation({
-      mutationFn: async (transactionData: any) => {
-        const response = await createRegisterTransaction(transactionData);
-        return response;
-      },
-      onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ["transactions"] });
-        queryClient.invalidateQueries({ queryKey: ["registers"] });
-      },
+        mutationFn: async (transactionData: any) => {
+            const response = await createRegisterTransaction(transactionData);
+            return response;
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["transactions"]});
+            queryClient.invalidateQueries({queryKey: ["registers"]});
+        },
     });
-  };
+};
